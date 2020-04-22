aaf5d06a66cca08914f139ab5a83668956b206d4
Vanderlei Loureiro <vandeloureiro@gmail.com>
2020-04-22 19:15:52 -0300

    correction in enrollment entity

--- src/main/java/com/br/eduriacore/model/Enrollment.java
+++ src/main/java/com/br/eduriacore/model/Enrollment.java
@@ -1,15 +1,31 @@
 package com.br.eduriacore.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "enrollment")
 public class Enrollment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="enrollment_id")
+    private String enrollmentId;
+
     @ManyToOne
     @JoinColumn(name = "student_id", nullable = false)
     private Student student;
+
+    private int level;
+
+    private int contentOrder;
 }
--- src/main/java/com/br/eduriacore/model/Student.java
+++ src/main/java/com/br/eduriacore/model/Student.java
@@ -2,11 +2,7 @@ package com.br.eduriacore.model;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -17,16 +13,7 @@ import lombok.Data;
 @Table(name = "student")
 public class Student extends Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="student_id")
-    private String studentId;
-
     @OneToMany(mappedBy = "student")
     private List<Enrollment> enrollments;
-
-    private int level;
-
-    private int contentOrder;
 
 }
