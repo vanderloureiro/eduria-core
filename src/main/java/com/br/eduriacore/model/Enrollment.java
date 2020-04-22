99fa2c0a5623262265568b1d7d239a28ce382f18
Vanderlei Loureiro <vandeloureiro@gmail.com>
2020-04-22 19:06:46 -0300

    relationship between enrollment and student

--- src/main/java/com/br/eduriacore/model/Enrollment.java
+++ src/main/java/com/br/eduriacore/model/Enrollment.java
@@ -1,11 +1,15 @@
 package com.br.eduriacore.model;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "enrollment")
 public class Enrollment {
 
-
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
 }
--- src/main/java/com/br/eduriacore/model/Student.java
+++ src/main/java/com/br/eduriacore/model/Student.java
@@ -2,7 +2,12 @@ package com.br.eduriacore.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -12,6 +17,16 @@ import lombok.Data;
 @Table(name = "student")
 public class Student extends Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="student_id")
+    private String studentId;
+
+    @OneToMany(mappedBy = "student")
     private List<Enrollment> enrollments;
+
+    private int level;
+
+    private int contentOrder;
 
 }
